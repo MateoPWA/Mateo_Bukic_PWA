66d4cb3a5f03a4acdc5b02e5e3fd3d2bec28a812
Mateo <angerfist.fans@gmail.com>
2019-11-16 17:12:16 +0100

    Second Commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <Hero msg="Find other riders" title="Welcome to Your Moto life App"/>
+    <pocetna msg="Find other riders around you" title="Welcome to Your Moto life App"/>
   </div>
 </template>
 
 <script>
-import Hero from './components/Hero.vue'
+import pocetna from './components/pocetna.vue'
 
 export default {
   name: 'app',
   components: {
-    Hero
+    pocetna
   }
 }
 </script>
